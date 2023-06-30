31228b628e1f74e843a055334394843b25126f13
Zoran Brbora <zoranbrbora1717@gmail.com>
2023-06-30 23:03:12 +0200

    sales

--- SOLUTIONS/chapter11.sql
+++ SOLUTIONS/chapter11.sql
@@ -5,3 +5,12 @@ SELECT products.productname,
 (SELECT MAX(orderdate)
 FROM orders) AS order_date
 FROM products
+
+/* “2. “List customers who ordered bikes.”*/
+SELECT customers.custfirstname, customers.custlastname
+FROM customers
+WHERE customers.customerid IN
+(SELECT orders.customerid FROM orders
+INNER JOIN order_details ON order_details.ordernumber = orders.ordernumber
+INNER JOIN products ON products.productnumber = order_details.productnumber
+WHERE products.productnumber IN (6));
