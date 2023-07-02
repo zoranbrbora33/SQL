44b6ba4a0c9c321c9b6af641ed29b98906cd918b
Zoran Brbora <zoranbrbora1717@gmail.com>
2023-07-02 22:30:03 +0200

    finished entertainment database

--- SOLUTIONS/chapter12.sql
+++ SOLUTIONS/chapter12.sql
@@ -15,3 +15,31 @@ FROM order_details AS od
 INNER JOIN products as p
 ON p.productnumber = od.productnumber
 WHERE ordernumber = 8;
+
+
+--- ENTERTAINMENT DATABASE ---
+
+/* “1. “What is the average salary of a booking agent?”*/
+SELECT ROUND(AVG(salary), 2) AS average_salary
+FROM agents
+
+/* “2. “Show me the engagement numbers for all engagements that have a contract price greater
+               than or equal to the overall average contract price.”*/
+SELECT engagementnumber AS engagement
+FROM engagements
+WHERE contractprice >=
+(SELECT AVG(contractprice)
+FROM engagements)
+
+/* “3. “How many of our entertainers are based in Bellevue?”*/
+SELECT COUNT(*)
+FROM entertainers
+WHERE entcity = 'Bellevue';
+
+/* “4. “Which engagements occur earliest in October 2017?”*/
+SELECT engagementnumber AS engagements
+FROM engagements
+WHERE startdate IN
+(SELECT MIN(startdate)
+FROM engagements
+WHERE EXTRACT(MONTH FROM startdate) = 10);
