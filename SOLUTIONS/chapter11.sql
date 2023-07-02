6575211ba9203e5c4e9c2bc4cab157702ee93d6e
Zoran Brbora <zoranbrbora1717@gmail.com>
2023-07-02 11:48:33 +0200

    finsihed school database

--- SOLUTIONS/chapter11.sql
+++ SOLUTIONS/chapter11.sql
@@ -57,3 +57,35 @@ FROM agents
 WHERE agents.agentid NOT IN
 (SELECT e.agentid 
 FROM engagements AS e);
+
+
+--- SCHOOL DATABASE ---
+
+/* “1. “List all staff members and the count of classes each teaches.”*/
+SELECT (stffirstname || ' ' || stflastname) AS staff,
+(SELECT COUNT(*)
+FROM faculty_classes AS fc
+WHERE staff.staffid = fc.staffid) AS num_of_classes
+FROM staff
+ORDER BY 2 DESC;
+
+/* “2. “Display students enrolled in a class on Tuesday.”*/
+
+--- There is a mistake in the solution from the book.
+--- Book solution lists all the students but only 16 students 
+--- are enrolled in classes on Tueasday
+SELECT (studfirstname || ' ' || studlastname) AS student
+FROM students
+WHERE students.studentid IN
+(SELECT DISTINCT studentid FROM student_schedules AS sc
+INNER JOIN classes AS c ON c.classid = sc.classid
+WHERE c.tuesdayschedule = 1 AND sc.classstatus = 1);
+
+/* “3. “List the subjects taught on Wednesday.”*/
+SELECT subjectname 
+FROM subjects
+WHERE subjects.subjectid IN
+(SELECT classes.subjectid 
+FROM classes
+WHERE classes.wednesdayschedule = 1)
+ORDER BY 1;
