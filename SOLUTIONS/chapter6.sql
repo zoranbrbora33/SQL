ee81d60139c4a62be58ce8d97530b8fec9414be8
Zoran Brbora <zoranbrbora1717@gmail.com>
2023-07-19 18:50:22 +0200

    Update chapter6.sql

--- SOLUTIONS/chapter6.sql
+++ SOLUTIONS/chapter6.sql
@@ -79,3 +79,71 @@ WHERE recipeclassid = 1 AND notes IS NOT NULL;
 /* “2. “Display the first five recipes.”*/
 SELECT recipetitle AS recipe FROM recipes
 LIMIT 5;
+
+
+--- MY SAMPLE STATEMENTS SOLUTIONS ---
+
+--- SALES DATABASE ---
+
+/* “Show me all the orders for customer number 1001.”*/
+SELECT ordernumber 
+FROM orders
+WHERE customerid = 1001;
+
+/* “Show me an alphabetized list of products with names that begin with ‘Dog’.”*/
+SELECT productname
+FROM products
+WHERE productname LIKE 'Dog%'
+ORDER BY productname ASC;
+
+
+--- ENTERTAINMENT DATBASE ---
+
+/* “Show me an alphabetical list of entertainers based in Bellevue, Redmond, or Woodinville.”*/
+SELECT entertainerid AS id, entstagename AS entertainer
+FROM entertainers
+WHERE entcity IN ('Bellevue', 'Redmond', 'Woodinville')
+ORDER BY entstagename ASC;
+
+/* “Show me all the engagements that run for four days.”*/
+SELECT * FROM engagements
+WHERE CAST(enddate - startdate AS INTEGER) = 3;
+
+
+--- SCHOOL DATABASE ---
+
+/* “Show me an alphabetical list of all the staff members and their salaries if they
+               make between $40,000 and $50,000 a year.”*/
+SELECT CONCAT(stffirstname, ' ', stflastname) AS staff
+FROM staff
+WHERE salary BETWEEN 40000 AND 50000;
+
+/* “Show me a list of students whose last name is ‘Kennedy’ or who live in Seattle.”*/
+SELECT CONCAT(studfirstname, ' ', studlastname) AS student
+FROM students
+WHERE studlastname = 'Kennedy' or studcity = 'Seattle';
+
+
+--- BOWLING DATABASE ---
+
+/* “List the ID numbers of the teams that won one or more of the first ten matches in
+               Game 3.”*/
+SELECT winningteamid 
+FROM match_games
+WHERE gamenumber = 3 AND matchid BETWEEN 1 AND 10;
+
+/* “List the bowlers in teams 3, 4, and 5 whose last names begin with the letter ‘H’.”*/
+SELECT CONCAT(bowlerfirstname, ' ', bowlerlastname) AS bowler
+FROM bowlers
+WHERE teamid IN (3, 4, 5) AND bowlerlastname LIKE 'H%';
+
+
+--- RECIPES DATABASE ---
+
+/* “List the recipes that have no notes.”*/
+SELECT * FROM recipes
+WHERE notes IS NULL;
+
+/* “Show the ingredients that are meats (ingredient class is 2) but that aren’t chicken.”*/
+SELECT * FROM ingredients
+WHERE ingredientclassid = 2 AND ingredientname NOT LIKE '%Chicken%';
